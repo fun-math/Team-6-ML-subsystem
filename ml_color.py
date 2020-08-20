448692b73d179b704ed9d892f340c801393697c2
fun-math <mallikakumar1729@gmail.com>
2020-08-20 16:50:22 +0530

    Added demo rosnodes

---
+++ ml_color.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+import tensorflow as tf 
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPool2D, BatchNormalization
+#import tensorflow_hub as hub
+import numpy as np 
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+graph = tf.get_default_graph()
+
+
+colours=['Green','Navy_Blue','Cyan','Red','Yellow','Brown','Black','Grey']
+h=48
+b=64
+
+
+inputs=tf.keras.Input(shape=(h,b,3))
+
+x=Conv2D(16,3,1,padding='same',activation='relu')(inputs)
+x=MaxPool2D(2,2,padding='valid')(x)
+
+x=Conv2D(32,3,1,padding='same',activation='relu')(x)
+x=BatchNormalization()(x)
+x=MaxPool2D(2,2,padding='valid')(x)
+
+x=Conv2D(64,3,1,padding='same',activation='relu')(x)
+x=MaxPool2D(2,2,padding='valid')(x)
+
+x=Conv2D(128,3,1,padding='same',activation='relu')(x)
+x=MaxPool2D(2,2,padding='valid')(x)
+
+x=Flatten()(x)
+output=Dense(8,activation='softmax')(x)
+
+model=tf.keras.Model(inputs,output,name='model1b')
+
+model.compile(optimizer='adagrad',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+PATH='/home/amit/Team-6-ML-subsystem/model1b_10000.h5'
+#model.load_weights(PATH)
+
+
+def callback(ros_image_message):
+	bridge = CvBridge()
+	img = bridge.imgmsg_to_cv2(ros_image_message, 
+	desired_encoding='passthrough')
+
+	#img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	img=cv2.resize(img,(b,h))
+	img=img.reshape(1,h,b,3)
+	#img=img/255.0
+	#print(img[0][0][0][0])
+	#img_new=cv2.resize(img,(64*10,48*10))
+	#cv2.imshow('f',img_new)
+	#cv2.waitKey(0)
+	#cv2.destroyAllWindows()
+	#model=tf.keras.models.load_model((PATH))
+	global graph
+	with graph.as_default():
+		pass
+		preds=model.predict([img])
+		result=np.argmax(preds,axis=1)
+
+	print(colours[int(result)])
+	print(preds[0][result])	
+
+def listener():
+	rospy.init_node('ml_color',anonymous=True)
+	rospy.Subscriber('/hexbot/camera1/image_raw',
+		Image,callback)
+	rospy.spin()
+
+if __name__=='__main__':
+	listener()
+
+
