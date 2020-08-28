00b0007548480889d0479ff11b07241ee2fa5533
fun-math <mallikakumar1729@gmail.com>
2020-08-29 00:51:28 +0530

    Add other utility files

--- cvshapes.py
+++ cvshapes.py
@@ -7,21 +7,43 @@ def load(img_name):
   img=cv2.imread(img_name,1)
   img=cv2.resize(img,(960,540))
   img_new=img[106:488,245:780]
-  #img_new=cv2.resize(img_new,(b,h))
-  #img_new=img_new.reshape(h,b,1)
   return img_new
 
 #if using solid work images and want to crop out some stuff
-img=load('cyan.png')
+cv_image=load('cyan.png')
 
 #Otherwise
-#img=cv2.imread('b.png')
+#cv_image=cv2.imread('b.png')
+
+gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+_,thresh = cv2.threshold(gray,230,255,cv2.THRESH_BINARY_INV)
+_, contours, hierarchy= cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+
+ind=0
+dist=800
+for i in range(len(contours)) :
+	M = cv2.moments(contours[i])
+	cx = int(M['m10']/M['m00'])
+	cy = int(M['m01']/M['m00'])
+	b=cv_image.shape[1]//2
+	if abs(b-cx)<dist :
+		dist=abs(b-cx)
+		ind=i
+
+cnt=contours[ind]
+x,y,w,h=cv2.boundingRect(cnt)
+cx=x+w//2
+cy=y+h//2
+img=cv_image[int(max(cy-1.2*h//2,0)):int(min(cy+1.2*h//2,800)),int(max(cx-1.2*w//2,0)):int(min(cx+1.2*w//2,800))]
+
+#img is the processed image which is a closely cropped image of the object at the centre with a padding of 0.4 times dimensions.
 
 img_rgb=img.copy()
 img_hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-_,thresh = cv2.threshold(gray,200,255,cv2.THRESH_BINARY_INV)
+_,thresh = cv2.threshold(gray,230,255,cv2.THRESH_BINARY_INV)
 _, contours, hierarchy= cv2.findContours(thresh,cv2.RETR_TREE,
 	cv2.CHAIN_APPROX_SIMPLE)
 #print(len(contours))
@@ -54,10 +76,10 @@ elif wr*hr>w*h+0.1*area :
 	shape="cone"
 elif w*1.0/h<1.5 :
 	shape="it's tall not wide"
-elif abs(0.786*a*b-area)<0.05*area:
+elif abs(0.786*a*b-area)<0.1*area:
 	shape="torus"
 else :
-	shape="I don't know"
+	shape="cylinder"
 
 mask=np.zeros_like(gray)
 mask=cv2.drawContours(mask,[cnt],0,255,-1)
@@ -66,6 +88,7 @@ cv2.imshow('mask',mask)
 mask=mask/255.0
 num=np.sum(mask)
 
+#average value of r,g,b over the contour
 r=np.sum(img_rgb[:,:,2]*mask)/num
 g=np.sum(img_rgb[:,:,1]*mask)/num
 b=np.sum(img_rgb[:,:,0]*mask)/num
@@ -73,23 +96,31 @@ b=np.sum(img_rgb[:,:,0]*mask)/num
 color="unidentified"
 
 if (r-g)**2+(g-b)**2+(b-r)**2<100 :
+	# if r,g,b are close enough, the colour is some shade of gray  
 	bright=np.sum(gray*mask)/num
-	if bright<10 :
+	if bright<20 :
 		color="black"
 	else :
 		color="gray"
 
 else :
 	img_hsv=img_hsv*1.0
+	# average hue value over the contour 
 	hue=np.sum(mask*img_hsv[:,:,0])/np.sum(mask)
-	if hue<=5 or hue>=175 :
+	if hue<=7 :
 		color="red"
+	elif hue<=23 :
+		color="brown"
+	elif hue<=45:
+		color="yellow"
+	elif hue<=75:
+		color="green"
+	elif hue<=105:
+		color="cyan"
+	elif hue<=150:
+		color="navy_blue"
 	else :
-		colors=["green","navy_blue","cyan","yellow","brown"]
-		hue_values=[60,120,90,30,15]
-		for i in range(5) :
-			if hue_values[i]-5<=hue<=hue_values[i]+5 :
-				color=colors[i]
+		color="red"
 
 print(shape)
 print(color)
--- ml_shape.py
+++ ml_shape.py
@@ -1,83 +1,20 @@
-#!/usr/bin/env python
-import rospy
-import cv2
 import tensorflow as tf 
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPool2D, BatchNormalization
-#import tensorflow_hub as hub
 import numpy as np 
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-
-graph = tf.get_default_graph()
+import cv2
 
 h=48
 b=64
+model=tf.keras.models.load_model('model1a_9927.h5')
 
-inputs=tf.keras.Input(shape=(h,b,1))
-
-x=Conv2D(16,3,1,padding='same',activation='relu')(inputs)
-x=MaxPool2D(2,2,padding='valid')(x)
-
-x=Conv2D(32,3,1,padding='same',activation='relu')(x)
-x=Conv2D(32,3,1,padding='same',activation='relu')(x)
-x=BatchNormalization()(x)
-x=MaxPool2D(2,2,padding='valid')(x)
-
-x=Conv2D(64,3,1,padding='same',activation='relu')(x)
-x=Conv2D(64,3,1,padding='same',activation='relu')(x)
-x=MaxPool2D(2,2,padding='valid')(x)
-
-x=Conv2D(128,3,1,padding='same',activation='relu')(x)
-x=MaxPool2D(2,2,padding='valid')(x)
-
-x=Flatten()(x)
-output=Dense(2,activation='softmax')(x)
-
-model=tf.keras.Model(inputs,output,name='model1a')
-
-model.compile(optimizer='adagrad',
-	loss='sparse_categorical_crossentropy',
-	metrics=['accuracy'])
-
-PATH='/home/amit/Team-6-ML-subsystem/model1a_9927.h5'
-#model.load_weights(PATH)
-
-
-def callback(ros_image_message):
-	bridge = CvBridge()
-	img = bridge.imgmsg_to_cv2(ros_image_message, 
-	desired_encoding='passthrough')
-
-	img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	img=cv2.resize(img,(b,h))
-	img=img.reshape(1,h,b,1)
-	img=img/255.0
-	#print(img[0][0][0][0])
-	#img_new=cv2.resize(img,(64*10,48*10))
-	#cv2.imshow('f',img_new)
-	#cv2.waitKey(0)
-	#cv2.destroyAllWindows()
-	#model=tf.keras.models.load_model((PATH))
-	global graph
-	with graph.as_default():
-		pass
-		preds=model.predict([img])
-		result=np.argmax(preds,axis=1)
-
-	print(result)
-	print(preds[0][result])	
-
-def listener():
-	rospy.init_node('ml',anonymous=True)
-	rospy.Subscriber('/hexbot/camera1/image_raw',
-		Image,callback)
-	rospy.spin()
-
-if __name__=='__main__':
-	listener()
-
-
-
-
-
-
+img=cv2.imread('cyan.png')
+img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img=cv2.resize(img,(b,h))
+img=img.reshape(1,h,b,1)
+img=img/255.0
+preds=model.predict(img)
+res=np.argmax(preds,axis=1)
+if res==0:
+	shape="torus"
+else :
+	shape="not torus"
+print(shape,preds[0][int(res)])
